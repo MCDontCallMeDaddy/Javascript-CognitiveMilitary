32ba6a62d0e267ed4bde7fda071b660513febcfc
MCDontCallMeDaddy <45108247+MCDontCallMeDaddy@users.noreply.github.com>
2019-02-05 13:58:20 -0500

    Add files via upload

---
+++ Mike.js
@@ -0,0 +1,38 @@
+class List {
+
+  constructor() {
+    this.list = [];
+  }
+
+  addItem (item) {              // console.log(addItem(list, item));
+    const {list} = this;
+    return [
+      ...this.list,
+      item
+    ];
+  }
+
+  removeItem (item) {            // console.log(removeItem(ist, list[0]));
+    const {list} = this;
+    return list.filter((listItem) => {
+      return (listItem.id !== item.id);
+    });
+  }
+
+  updateItem (itemId, newValue) {  
+    const {list} = this;                     // console.log(updateItem(list, list[0].id, newValue))
+    return list.map((listItem) => {
+      return (listItem.id === itemId) ? {...listItem, name: newValue} : listItem;
+    });
+  }
+
+  getItem (itemId) {  
+    const {list} = this;             // console.log(getItem(list, list[0].id));
+    return list.find((listItem) => {
+      return (listItem.id === itemId);
+    });
+  }
+}
+
+const myList = new List();
+myList.addItem()
